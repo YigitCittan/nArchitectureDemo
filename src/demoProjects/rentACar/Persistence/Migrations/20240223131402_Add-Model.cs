580b1a0407d41488a58a3ca62adae8ef1f4b6b55
Yigit Cittan <yigit.cittan00@gmail.com>
2024-02-23 22:37:15 +0300

    Featrues Models Added

---
+++ src/demoProjects/rentACar/Persistence/Migrations/20240223131402_Add-Model.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class AddModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "name",
+                table: "Brands",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Brands",
+                newName: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Models",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BrandId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DailyPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Models", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Models_Brands_BrandId",
+                        column: x => x.BrandId,
+                        principalTable: "Brands",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Models",
+                columns: new[] { "Id", "BrandId", "DailyPrice", "ImageUrl", "Name" },
+                values: new object[] { 1, 1, 8500m, "", "420d" });
+
+            migrationBuilder.InsertData(
+                table: "Models",
+                columns: new[] { "Id", "BrandId", "DailyPrice", "ImageUrl", "Name" },
+                values: new object[] { 2, 1, 6500m, "", "320i" });
+
+            migrationBuilder.InsertData(
+                table: "Models",
+                columns: new[] { "Id", "BrandId", "DailyPrice", "ImageUrl", "Name" },
+                values: new object[] { 3, 2, 4500m, "", "A180" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Models_BrandId",
+                table: "Models",
+                column: "BrandId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Models");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Brands",
+                newName: "name");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Brands",
+                newName: "id");
+        }
+    }
+}
